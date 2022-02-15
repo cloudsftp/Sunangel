900ba0cc247bf59466d282ad61b2c82e7dab6d87
Fabian Weik <fabian.weik@protonmail.com>
2022-02-15 19:41:20 +0100

    sunangle | implement altitude and azimut sun angle

--- sunangel/sunangel.go
+++ sunangel/sunangel.go
@@ -6,7 +6,27 @@ import (
 )
 
 func AltitudeSunAngleAt(date time.Time, latitude, longitude float64) float64 {
-	return math.Asin(
-		float64(0),
-	)
+	delta := declinationOfSunAt(date)
+	tau := hourAngleOfSunAt(date, longitude)
+
+	argument := math.Cos(delta) * math.Cos(tau) * math.Cos(latitude)
+	argument += math.Sin(delta) * math.Sin(latitude)
+
+	return math.Asin(argument)
+}
+
+func AzimutSunAngleAt(date time.Time, latitude, longitude float64) float64 {
+	delta := declinationOfSunAt(date)
+	tau := hourAngleOfSunAt(date, longitude)
+
+	nominator := math.Sin(tau)
+	denominator := math.Cos(tau) * math.Sin(latitude)
+	denominator -= math.Tan(delta) * math.Cos(latitude)
+
+	argument := nominator / denominator
+	result := math.Atan(argument)
+	if denominator < 0 {
+		result += math.Pi
+	}
+	return result
 }
--- test/sunangel_test.go
+++ test/sunangel_test.go
@@ -3,12 +3,20 @@ package test
 import (
 	"testing"
 
+	"github.com/cloudsftp/Sunangel/angle"
 	"github.com/cloudsftp/Sunangel/sunangel"
 )
 
 func TestAltitudeSunangleAt(t *testing.T) {
 	got := sunangel.AltitudeSunAngleAt(dateWiki, latitudeWiki, longitudeWiki)
-	want := float64(19.062)
+	want := angle.RadiansFromDegrees(19.062)
+
+	assertApproxEqual(t, got, want)
+}
+
+func TestAzimutSunAngleAt(t *testing.T) {
+	got := sunangel.AzimutSunAngleAt(dateWiki, latitudeWiki, longitudeWiki)
+	want := angle.RadiansFromDegrees(265.938)
 
 	assertApproxEqual(t, got, want)
 }
