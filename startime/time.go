d561a7293ae0ca49a6f4c90a129b0744f8fdbffb
Fabian Weik <fabian.weik@protonmail.com>
2022-04-08 20:24:30 +0200

    ci | test convert date to UTC in TimeOfDayAsDecimal

--- startime/time.go
+++ startime/time.go
@@ -44,8 +44,10 @@ func JulianCenturiesSince2000ToMidnightOf(date time.Time) float64 {
 	return t0
 }
 
-// TimeOfDayAsDecimal returns the time of the day of the goven time as a decimal number.
+// TimeOfDayAsDecimal returns the time of the day of the given time as a decimal number.
 func TimeOfDayAsDecimal(date time.Time) float64 {
+	date = date.UTC()
+
 	timeOfDay := float64(date.Hour())
 	timeOfDay += float64(date.Minute()) / 60
 	timeOfDay += float64(date.Second()) / 3600
